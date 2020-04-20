a643ce3bb3be279de7259bf237ae03efbba32383
Roberto <ruffinengo@lochiva.com>
2020-04-20 10:33:34 +0200

    create first() method

---
+++ src/Controller/LearningController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller ;
+
+class LearningController extends AppController
+{
+
+    public function first(){
+
+        die('nel metodo first di LearningController');
+
+    }
+
+
+
+}
